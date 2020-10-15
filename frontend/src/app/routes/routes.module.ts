96ea6ff637e70109b21b031c624d49d00ff03931
Manh Ling Vu <vumanhling@icloud.com>
2020-10-15 10:22:00 +0200

    Add routes

--- frontend/src/app/routes/routes.module.ts
+++ frontend/src/app/routes/routes.module.ts
@@ -3,16 +3,23 @@ import { RouterModule, Routes } from '@angular/router';
 import { MaterialModule } from '@app/shared/material.module';
 import { ComponentsModule } from '@components/components.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { BrowseComponent } from './browse/browse.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HistoryComponent } from './history/history.component';
 
 const routes: Routes = [
   {
     path: '',
     component: DashboardComponent,
   },
+  { path: 'dashboard',   redirectTo: '', pathMatch: 'full' },
+  { path: 'browse',   component: BrowseComponent },
+  { path: 'history',   component: BrowseComponent },
+  { path: 'profile',   component: ProfileComponent },
 ];
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, BrowseComponent, ProfileComponent, HistoryComponent],
   imports: [RouterModule.forRoot(routes), MaterialModule, ComponentsModule],
   exports: [RouterModule],
 })
